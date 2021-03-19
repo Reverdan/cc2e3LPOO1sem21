77c2e280d1678269101e54c5a41ea9c32099cfcd
reverdan <reverdan@gmail.com>
2021-03-19 19:08:21 -0300

    Atualização
    Classes controle, validacao e calcular implementadas

--- Soma/src/modelo/Controle.java
+++ Soma/src/modelo/Controle.java
@@ -10,7 +10,25 @@ public class Controle
     
     public void executar()
     {
-        
+        this.mensagem = "";
+        Validacao validacao = new Validacao();
+        validacao.num1 = this.num1;
+        validacao.num2 = this.num2;
+        validacao.operacao = this.operacao;
+        validacao.validar();
+        if (validacao.mensagem.equals(""))
+        {
+            Calcular calcular = new Calcular();
+            calcular.numero1 = validacao.numero1;
+            calcular.numero2 = validacao.numero2;
+            calcular.operacao = this.operacao;
+            calcular.resolver();
+            this.resultado = calcular.resultado;
+        }
+        else
+        {
+            this.mensagem = validacao.mensagem;
+        }
     }
 }
 
