392fd2e52db5ee5005d3a949749025fc112a2ca1
reverdan <reverdan@gmail.com>
2021-03-19 22:12:22 -0300

    Atualização
    Utilizando construtor, encapsulamento, substituição de string, formatação de casas decimais.

--- Soma/src/modelo/Calcular.java
+++ Soma/src/modelo/Calcular.java
@@ -1,21 +1,40 @@
 package modelo;
 
+import java.text.DecimalFormat;
+
 public class Calcular
 {
-    public Double numero1;
-    public Double numero2;
-    public String operacao;
-    public String resultado;
+    private Double numero1;
+    private Double numero2;
+    private String operacao;
+    private String resultado;
 
-    public void resolver()
+    public Calcular(Double numero1, Double numero2, String operacao)
     {
-        if (operacao.equals("+"))
-            resultado = String.valueOf(numero1 + numero2);
-        if (operacao.equals("-"))
-            resultado = String.valueOf(numero1 - numero2);
-        if (operacao.equals("*"))
-            resultado = String.valueOf(numero1 * numero2);
-        if (operacao.equals("/"))
-            resultado = String.valueOf(numero1 / numero2);
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+        this.operacao = operacao;
+        this.resolver();
     }
+
+    private void resolver()
+    {
+        DecimalFormat df = new DecimalFormat("0.00");
+        
+        if (operacao.equals("+"))
+            resultado = df.format(numero1 + numero2);
+        if (operacao.equals("-"))
+            resultado = df.format(numero1 - numero2);
+        if (operacao.equals("*"))
+            resultado = df.format(numero1 * numero2);
+        if (operacao.equals("/"))
+            resultado = df.format(numero1 / numero2);
+    }
+
+    public String getResultado()
+    {
+        return resultado;
+    }
+    
+    
 }
