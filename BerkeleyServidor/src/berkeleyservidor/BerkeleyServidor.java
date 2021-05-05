5598f11d58e0c4f4846790688fe40e97f86b66c0
reverdan <reverdan@gmail.com>
2021-05-04 21:39:15 -0300

    Socket Berkeley
    Cliente e servidor básico

---
+++ BerkeleyServidor/src/berkeleyservidor/BerkeleyServidor.java
@@ -0,0 +1,32 @@
+package berkeleyservidor;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class BerkeleyServidor
+{
+
+    public static void main(String[] args) throws IOException
+    {
+        ServerSocket servidor = new ServerSocket(12345);
+        System.out.println("Porta 12345 aberta");
+        
+        Socket cliente = servidor.accept();
+        System.out.println("Nova conexão com o cliente " + 
+                cliente.getInetAddress().getHostAddress());
+        
+        Scanner s = new Scanner(cliente.getInputStream());
+        while(s.hasNextLine())
+        {
+            System.out.println(s.nextLine());
+        }
+        
+        s.close();
+        servidor.close();
+        cliente.close();
+        
+    }
+    
+}
